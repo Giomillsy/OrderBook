ca1016e1bf6b98a8ccc26ff7715fbd4c142e2765
Giomillsy <owenmillsy@gmail.com>
2025-06-02 16:52:10 +0100

    Minor updates to build.hpp

--- src/structures/Book.hpp
+++ src/structures/Book.hpp
@@ -4,16 +4,20 @@
 #include <map>
 #include <queue>
 #include <deque>
+
 #include "Order.hpp"
 
 
 class Book{
-// The order book, which contains and manages the orders
+// The order book, which contains and manages the orders constructed by the Order class
 private:
     
+    // Creating an alias type for both sides of the order book
     using LimitSellMap = std::map<double, std::deque<Order>, std::less<>>;
     using LimitBuyMap  = std::map<double, std::deque<Order>,std::greater<>>;
-
+    
+    // Each side of the order book is a map organised price levels
+    // With each price level being FIFO queue.
     LimitSellMap limitSell;
     LimitBuyMap  limitBuy;
 
@@ -21,16 +25,16 @@ private:
     template <typename Comparator>
     void showLimit(const std::map<double, std::deque<Order>,Comparator>& limitBook);
 
+    // Market matching logic. For crossing two orders
     virtual void marketMatch(Order& o);
 
 public:
 
-    //Destructor
     virtual ~Book() = default;
 
-friend struct BookTestHelper; //Helper for unit testing
+    friend struct BookTestHelper; //Helper for unit testing
 
-    void addOrder(Order& o);
+    void addOrder(Order& o); //Adds an order to the Limit book.
 
     void showOrders(); // Prints orders on both side of the book
     
